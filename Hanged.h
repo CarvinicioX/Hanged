0b0bee819a38005605dad314e6e3feb3b13e65bd
CarvinicioX <carvinicio@yahoo.com>
2015-02-22 20:17:03 -0600

    Update Hanged.h

--- Hanged.h
+++ Hanged.h
@@ -1,1 +1,30 @@
+#ifndef HANGED_H
+#define HANGED_H
+#include <string>
+#include <vector>
 
+using namespace std;
+
+class Hanged{
+private:
+	vector<string> newlist;
+	vector<string> oldlist;
+	string player;
+	int cats;
+	int intentos;
+	int levels;
+	string getPalabra;
+	string setPalabra;
+public:
+	Hanged(string, int, int);
+	void Iniciar(int);
+	string Otra();
+	void Level(int);
+	bool Termino()const;
+	string getStringCurrentState();
+	string Guess(char);
+	bool Guess(string);
+	bool Gano()const;
+};
+
+#endif	/* HANGED_H */
